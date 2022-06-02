03fe70d6c6984ba4f89c1623500259a4e20a6ee4
danushika.priyadarshani <danushikadan@gmail.com>
2022-06-02 22:15:47 +0530

    Project creation

---
+++ YourNews/Shared/YourNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourNewsApp.swift
+//  Shared
+//
+//  Created by Danushika Priyadarshani on 2022-06-02.
+//
+
+import SwiftUI
+
+@main
+struct YourNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
